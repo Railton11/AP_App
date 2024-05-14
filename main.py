f20c7573801a422cda655e9ca61861bcc87dd88a
Railton <nicolaufeitosa2222@gmail.com>
2024-05-14 20:30:42 -0300

    refatorando main

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from tela import Tela
 
-if __name__ == '__main__':
-    Tela().run()
+if __name__ == "__main__":
+    try:
+        app = Tela()
+        app.run()
+    except ImportError:
+        print("Erro de execução grave, por favor entre em contato com o desenvolvedor para resolução deste problema.")
+    except Exception:
+        print("Ocorreu um erro inesperado, por favor entre em contato com o desenvolvedor.")
